230a3231f3ec4642213c6aee4a135af635b530cf
Alex Spangher <spangher@usc.edu>
2024-07-03 15:32:27 -0400

    final updates, let's run

--- DATASET/yt-aria.py
+++ DATASET/yt-aria.py
@@ -11,11 +11,6 @@ from dtw import run as run_dtw
 
 MODEL_NAME = "medium-stacked"
 CHECKPOINT_NAME = f"piano-medium-stacked-1.0"
-START_ID = 0
-END_ID = -1
-FILE_PATH = ""
-MIDI_PATH = "./midi"
-MP3_PATH = "./mp3"
 
 
 def parse_args():
@@ -23,44 +18,24 @@ def parse_args():
     Parse command line arguments to set the start index, end index, and file path
     for the JSON file containing links.
     """
-    global START_ID
-    global END_ID
-    global FILE_PATH
-    global MIDI_PATH
-    global MP3_PATH
-
     parser = argparse.ArgumentParser(
         prog="yt-aria-script",
         description="runs yt-dlp & aria-amt to extract midi for json file of links"
     )
-    parser.add_argument("json")
-    parser.add_argument("-s", "--start-idx", type=int)
-    parser.add_argument("-e", "--end-idx", type=int)
-    parser.add_argument("-m", "--midi-dir", type=str)
-    parser.add_argument("-t", "--mp3-dir", type=str)
+    parser.add_argument("--input_json", type=str, required=True)
+    parser.add_argument("-s", "--start-idx", type=int, default=0)
+    parser.add_argument("-e", "--end-idx", type=int, default=-1)
+    parser.add_argument("-m", "--midi-dir", type=str, default="midi")
+    parser.add_argument("-t", "--mp3-dir", type=str, default="mp3")
     parser.add_argument("--dtw-file", type=str, default="dtw-scores.csv")
     parser.add_argument("-c", "--seed", type=int)
     args = parser.parse_args()
-    
-    if args.start_idx is not None:
-        START_ID = args.start_idx
-    if args.end_idx is not None:
-        END_ID = args.end_idx
-    if args.midi_dir is not None:
-        MIDI_PATH = args.download_dir
-    if args.mp3_dir is not None:
-        MP3_PATH = args.download_dir
     if args.seed is not None:
         random.seed(args.seed)
-
-    if os.path.isfile(args.json):
-        FILE_PATH = str(args.json)
-    else:
-        print("ERROR: json file not recognized")
-        exit()
+    return args
 
 
-def get_name(name, suffix="mp3"):
+def get_name(name, path, suffix="mp3"):
     """
     Generate a file name using the provided name, index, and suffix.
 
@@ -71,7 +46,7 @@ def get_name(name, suffix="mp3"):
     Returns:
     str: The generated file name.
     """
-    return (f"audio-{name}.{suffix}")
+    return os.path.join(path, f"audio-{name}.{suffix}")
 
 
 def load_json(json_file):
@@ -124,28 +99,7 @@ def load_set_from_json(json_file, start_index, end_index):
     return links
 
 
-def download_set_from_json(yt_links, start_index, end_index):
-    """
-    Download a set of audio files from YouTube links using yt-dlp.
-
-    Parameters:
-    yt_links (list): A list of YouTube URLs.
-    start_index (int): The starting index for the download.
-    end_index (int): The ending index for the download.
-    """
-    print("downloading links..")
-    for i in range(start_index, end_index):
-        try:
-            x = yt_links[i]
-        except:
-            print("ERROR: requested link is out of range")
-            break
-        file_name = get_name(urllib.parse.quote(yt_links[i], safe='', encoding=None, errors=None))
-        os.system(f"yt-dlp --extract-audio --audio-format mp3 --no-playlist --audio-quality 0 {yt_links[i]} -o {MP3_PATH}/{file_name}")
-        print("downloaded audio for video: " + yt_links[i] + " as " + file_name)
-
-
-def download_using_ytdl(yt_links, i, name=None):
+def download_using_ytdl(yt_link, mp3_output_fp):
     """
     Download a single audio file from a YouTube link using yt-dlp.
 
@@ -157,39 +111,13 @@ def download_using_ytdl(yt_links, i, name=None):
     Returns:
     bool: True if the download is successful, False otherwise.
     """
-    if name is None:
-        name = i
-    print("downloading links..")
-    try:
-        x = yt_links[i]
-    except:
-        print("ERROR: requested link is out of range")
-        return False
-    file_name = get_name(name)
-    path = f"{MP3_PATH}/{file_name}"
-    if not os.path.isfile(path):
-        os.system(f"yt-dlp --extract-audio --audio-format mp3 --no-playlist --audio-quality 0 {yt_links[i]} -o {path}")
-        print("downloaded audio for video: " + yt_links[i])
+    if not os.path.isfile(mp3_output_fp):
+        os.system(f"yt-dlp --extract-audio --audio-format mp3 --no-playlist --audio-quality 0 {yt_link} -o {mp3_output_fp}")
+        print("downloaded audio for video: " + yt_link)
     else:
-        print("audio already downloaded for video: " + yt_links[i] + " as " + file_name)
+        print("audio already downloaded for video: " + yt_link + " as " + mp3_output_fp)
     return True
 
-
-def download_link(link, name):
-    """
-    Download a single audio file from a YouTube link using yt-dlp.
-
-    Parameters:
-    link (str): The YouTube URL.
-    name (str): The custom name for the file.
-
-    Returns:
-    bool: True if the download is successful, False otherwise.
-    """
-    print("downloading links..")
-    os.system(f"yt-dlp --extract-audio --audio-format mp3 --no-playlist --audio-quality 0 {link} -o {MP3_PATH}/{get_name(name)}")
-    print("downloaded audio for video: " + link)
-    return True
 
 
 def aria_amt_set_up():
@@ -199,12 +127,15 @@ def aria_amt_set_up():
     if not os.path.isfile(f"{CHECKPOINT_NAME}.safetensors"):
         # NOTE: uncomment or comment depending on system
         os.system(f"wget https://storage.googleapis.com/aria-checkpoints/amt/{CHECKPOINT_NAME}.safetensors")
-        # subprocess.run(["powershell", f"wget https://storage.googleapis.com/aria-checkpoints/amt/{CHECKPOINT_NAME}.safetensors"])
+        # subprocess.run([
+        #       "powershell",
+        #       f"wget https://storage.googleapis.com/aria-checkpoints/amt/{CHECKPOINT_NAME}.safetensors"
+        # ])
     else:
         print(f"Checkpoint already exists at {CHECKPOINT_NAME} - skipping download")
 
 
-def run_aria_amt(path, directory="."):
+def run_aria_amt(mp3_fp, directory="."):
     """
     Run aria-amt to transcribe audio files to MIDI.
 
@@ -213,53 +144,32 @@ def run_aria_amt(path, directory="."):
     directory (str): The directory to save the MIDI files (default is current directory).
     """
     os.system(
-        f"aria-amt transcribe {MODEL_NAME} {CHECKPOINT_NAME}.safetensors -load_path=\"{MP3_PATH}/{path}\" -save_dir=\"{directory}\" -bs=1"
+        f"aria-amt transcribe {MODEL_NAME} {CHECKPOINT_NAME}.safetensors -load_path=\"{mp3_fp}\" -save_dir=\"{directory}\" -bs=1"
     )
 
 
-def remove_mp3(name):
+def remove_mp3(mp3_fp):
     """
     Remove the downloaded MP3 file.
 
     Parameters:
     name (str): The base name for the file.
     """
-    file_name = get_name(name)
-    path = MP3_PATH + "/" + file_name
-    if (os.path.isfile(path)):
-        os.system(f"rm {path}")
-        print(f"{path} removed")
+    if (os.path.isfile(mp3_fp)):
+        os.system(f"rm {mp3_fp}")
+        print(f"{mp3_fp} removed")
     else:
         print(f"ERROR: mp3 file not found, could not remove")
 
 
-def get_link_from_file(file_name):
-    """
-    Extract the original URL from the downloaded file name.
-
-    Parameters:
-    file_name (str): The name of the downloaded file.
-
-    Returns:
-    str: The original URL.
-    """
-    s = file_name.split("-")
-    s = s[2].split(".")
-    print(s[0])
-    return urllib.parse.unquote_plus(s[0])
-
-
 def main():
-    global START_ID
-    global END_ID
+    args = parse_args()
 
-    parse_args()
-
-    if END_ID == -1:
-        links = load_json(FILE_PATH)
-        END_ID = len(links)
+    if args.end_idx < 0:
+        links = load_json(args.input_json)
+        args.end_idx = len(links)
     else:
-        links = load_set_from_json(FILE_PATH, START_ID, END_ID)
+        links = load_set_from_json(args.input_json, args.start_idx, args.end_idx)
 
     random.shuffle(links)
     aria_amt_set_up()
@@ -268,34 +178,28 @@ def main():
         print(f"{CHECKPOINT_NAME}.safetensors did not install")
         exit()
     
-    if not os.path.isdir(MP3_PATH):
-        os.system("mkdir " + MP3_PATH)
+    if not os.path.isdir(args.mp3_dir):
+        os.system("mkdir " + args.mp3_dir)
     
-    if not os.path.isdir(MIDI_PATH):
-        os.system("mkdir " + MIDI_PATH)
+    if not os.path.isdir(args.mid_dir):
+        os.system("mkdir " + args.mid_dir)
 
     BATCH = 2
     buffer_audio_files = []
     buffer_midi_files = []
-    for i in tqdm(range(START_ID, END_ID)):
+    for i in tqdm(range(args.start_idx, args.end_idx)):
         try:
             name = urllib.parse.quote(links[i], safe='', encoding=None, errors=None)
-            mid_fn = get_name(name, "mid")
-            mp3_fn = get_name(name, "mp3")
-            if not os.path.isfile(f"{MIDI_PATH}/{mid_fn}"):
-                if download_using_ytdl(links, i, name):
-                    run_aria_amt(mp3_fn, MIDI_PATH)
-                    if os.path.isfile(f"{MIDI_PATH}/{mid_fn}") and os.path.isfile(f"{MP3_PATH}/{mp3_fn}"):
+            mid_fn = get_name(name, args.mid_path, "mid")
+            mp3_fn = get_name(name, args.mp3_path, "mp3")
+            if not os.path.isfile(mid_fn):
+                if download_using_ytdl(links[i], mp3_fn):
+                    run_aria_amt(mp3_fn, args.mid_dir)
+                    if os.path.isfile(mid_fn) and os.path.isfile(mp3_fn):
                         buffer_audio_files.append(mp3_fn)
                         buffer_midi_files.append(mid_fn)
             if len(buffer_audio_files) >= BATCH:
-                #         output_file,
-                #         audio_dir,
-                #         mid_dir,
-                #         audio_file_list=None,
-                #         midi_file_list=None,
-                #         output_mode="a"
-                run_dtw()
+                run_dtw(args.dtw_file, args.mp3_dir, args.mid_dir, buffer_audio_files, buffer_midi_files)
 
                 list(map(remove_mp3, buffer_audio_files))
                 buffer_audio_files = []
